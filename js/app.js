9a6f53c954fcbd06f0effb64bda93be95624a022
ranaalmalki <ranaalmalki12@gmail.com>
2020-02-03 16:25:21 +0300

    update the winner function and reset function

--- js/app.js
+++ js/app.js
@@ -1,96 +1,28 @@
-//call the all button
-
-// let player = $(".box");
-// let player1 = "X";
-// let player2="O";
-// //every click will apper the text  
-//  player.click(function () {
-//    if($(this).text()=="X"){
-//     player.click(function () {
-
-//       if($(this).text()=="O"){
-//         player.click(function () {
-
-//           if($(this).text()=="X"){
-//             player.click(function () {
-
-//               if($(this).text()=="O"){
-//                 player.click(function () {
-
-//                   if($(this).text()=="X"){
-//                     player.click(function () {
-
-//                       if($(this).text()=="O"){
-//                         player.click(function () {
-
-//                           if($(this).text()=="X"){
-//                             player.click(function () {
-//                              return $(this).text()=="O";
-//                               if($(this).text()=="O"){
-//                                 player.click(function () {
-
-//                                   if($(this).text()=="X"){
-
-//                                   }
 
 
-//                                     }
-//                                );
-//                               }
+let playerTurn1 = $("#player-1");
+let playerTurn2 = $("#player-2");
 
-
-//                                 }
-//                            );
-//                           }
-
-
-//                             }
-//                        );
-//                       }
-
-
-//                         }
-//                    );
-//                   }
-
-
-//                     }
-//                );
-//               }
-
-
-//                 }
-//            );
-//           }
-
-
-//             }
-//        );
-//       }
-
-
-//         }
-//    );
-
-//    }
-
-//      }
-// );
 
 // plyer click button "X" Or "O"
 let player = $(".box");
 let player1 = "X";
 player.one("click", function () {
-  //for(let i = 0;i<player.length;i++){
+
   if (player1 === "X") {
     $(this).text("X");
     player1 = "O";
+   // playerTurn1.text(" x");
+
   } else {
     $(this).text("O");
     player1 = "X";
+    //playerTurn2.text(" O")
+
   }
   playerWin();
-  // }
+
+
 });
 
 // varibale to call the the id for the button 
@@ -106,89 +38,88 @@ let playerIndex9 = $("#box-9");
 
 // function to check the win 
 function playerWin() {
- 
-  if (playerIndex1.text()=="X" && playerIndex2.text()=="X" && playerIndex3.text()=="X" || playerIndex1.text()=="O" && playerIndex2.text()=="O" && playerIndex3.text()=="O") {
+
+  if (playerIndex1.text() == "X" && playerIndex2.text() == "X" && playerIndex3.text() == "X" || playerIndex1.text() == "O" && playerIndex2.text() == "O" && playerIndex3.text() == "O") {
     console.log("win");
-alert("you are the win");
+    player.off("click");
+
+    alert("winnner");
+
+
     //console.log(playerIndex1.text()=="X"+" 1");
-//console.log(playerIndex2.text()=="X"+"  2");
-  } else if (playerIndex4.text()=="X" && playerIndex5.text()=="X" && playerIndex6.text()=="X" || playerIndex4.text()=="O" && playerIndex5.text()=="O" && playerIndex6.text()=="O") {
+    //console.log(playerIndex2.text()=="X"+"  2");
+  } else if (playerIndex4.text() == "X" && playerIndex5.text() == "X" && playerIndex6.text() == "X" || playerIndex4.text() == "O" && playerIndex5.text() == "O" && playerIndex6.text() == "O") {
     console.log("win");
+    alert("winner");
+    player.off("click");
 
-  } else if (playerIndex7.text()=="X" && playerIndex8.text()=="X" && playerIndex9.text()=="X" || playerIndex7.text()=="O" && playerIndex8.text()=="O" && playerIndex9.text()=="O") {
+  } else if (playerIndex7.text() == "X" && playerIndex8.text() == "X" && playerIndex9.text() == "X" || playerIndex7.text() == "O" && playerIndex8.text() == "O" && playerIndex9.text() == "O") {
     console.log("win");
+    alert("winner");
 
-  } else if (playerIndex1.text()=="X" && playerIndex4.text()=="X" && playerIndex7.text()=="X" || playerIndex1.text()=="O" && playerIndex4.text()=="O" && playerIndex7.text()=="O") {
-    console.log("win");
+    player.off("click");
 
-  } else if (playerIndex2.text()=="X" && playerIndex5.text()=="X" && playerIndex8.text()=="X" || playerIndex2.text()=="O" && playerIndex5.text()=="O" && playerIndex8.text()=="O") {
+  } else if (playerIndex1.text() == "X" && playerIndex4.text() == "X" && playerIndex7.text() == "X" || playerIndex1.text() == "O" && playerIndex4.text() == "O" && playerIndex7.text() == "O") {
     console.log("win");
+    alert("winner");
 
-  } else if (playerIndex3.text()=="X" && playerIndex6.text()=="X" && playerIndex9.text()=="X" || playerIndex3.text()=="O" && playerIndex6.text()=="O" && playerIndex9.text()=="O") {
-    console.log("win");
+    player.off("click");
 
-  } else if (playerIndex1.text()=="X" && playerIndex5.text()=="X" && playerIndex9.text()=="X" || playerIndex1.text()=="O" && playerIndex5.text()=="O" && playerIndex9.text()=="O") {
+  } else if (playerIndex2.text() == "X" && playerIndex5.text() == "X" && playerIndex8.text() == "X" || playerIndex2.text() == "O" && playerIndex5.text() == "O" && playerIndex8.text() == "O") {
     console.log("win");
+    alert("winner");
 
-  } else if (playerIndex3.text()=="X" && playerIndex5.text()=="X" && playerIndex7.text()=="X" || playerIndex3.text()=="O" && playerIndex5.text()=="O" && playerIndex7.text()=="O") {
+    player.off("click");
+
+  } else if (playerIndex3.text() == "X" && playerIndex6.text() == "X" && playerIndex9.text() == "X" || playerIndex3.text() == "O" && playerIndex6.text() == "O" && playerIndex9.text() == "O") {
     console.log("win");
+    alert("winner");
+
+    player.off("click");
+
+  } else if (playerIndex1.text() == "X" && playerIndex5.text() == "X" && playerIndex9.text() == "X" || playerIndex1.text() == "O" && playerIndex5.text() == "O" && playerIndex9.text() == "O") {
+    console.log("win");
+    alert("winner");
+
+    player.off("click");
+
+  } else if (playerIndex3.text() == "X" && playerIndex5.text() == "X" && playerIndex7.text() == "X" || playerIndex3.text() == "O" && playerIndex5.text() == "O" && playerIndex7.text() == "O") {
+    console.log("win");
+    alert("winner");
+
+    player.off("click");
 
   } else {
     console.log("Tie");
   }
-} 
-let resetButton = $("#reset");
-function playeAgain(){
-
 }
-// let chose =[$("#box-1"),
-// $("#box-2"),
-// $("#box-3"),
-// $("#box-4"),
-// $("#box-5"),
-// $("#box-6"),
-// $("#box-7"),
-// $("#box-8")
-// ];
-// let player = $(".box");
-// function check(){
-//     let count = 0;
-//     this.player1 +="X";
-//     for(let i=0; i<player.length;i++){
-//         if( player[i].player1){
-// count++;
-//         }
-//     }
-// }
-// for(let i=0; i<player.length;i++){
+let resetButton = $("#reset");
+resetButton.click(function () {
+ 
+player.text("");
+player.one("click", function () {
+  if (player1 === "X") {
+    $(this).text("X");
+    player1 = "O";
+    //playerTurn1.text(" x");
 
-// player[i].on("click",check);
-// }
-// player.on("click",check);
-// let player1 = 1;
-// let player2=2;
-// let box = $(".box");
-// let turnPlayer1 = $("#player-1");
-// let turnPlayer2 = $("#player-2");
-// $(".box").click(function(){
+  } else {
+    $(this).text("O");
+    player1 = "X";
+    //playerTurn2.text(" O")
 
-//     box = 
-// });
-// const nextPlayer = function(turnPlayer1,player1){
-// if(player1 === 1){
-//     return turnPlayer1.html("turn player"+player1)
-// }else {
-//     return turnPlayer2.html("turn plyer"+player2)
-// }
-// }
-// const indexPlayer = function(){
-// if(player1 === 1){
-//     return "X";
-// }else{
-//     return "O"
-// }
-// }
+  }
+  playerWin();
 
-// let player =$(".box");
-// let player1 = "X";
-// let player2 = "O";
+
+});
+
+});
+
+
+
+
+
+
+
+
